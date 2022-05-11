a30d8972ea886a31d45291f541778fc3eda86b52
Aishwarya-Mirashi <80153834+Aishwarya-Mirashi@users.noreply.github.com>
2022-05-11 16:47:05 +0530

    Add files via upload

---
+++ HouseOnRent.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import screenLoader from "../images/screenLoad.gif";
+import rentHouseAction from "../store/Actions/rentHouseActions";
+import "../StyleSheet/HouseListed.css";
+import { Link } from "react-router-dom";
+const HouseOnRent = () => {
+  
+  return (
+    <div>Mess</div>
+  );
+};
+
+export default HouseOnRent;
---
+++ StarRating.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StarRating = () =>{
+    return <div>star</div>
+};
+
+export default StarRating;
